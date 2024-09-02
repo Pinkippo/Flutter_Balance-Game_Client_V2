627382e700819ab3ea6f575d7e46cb6f9f7192c6
SuuuHyeon <tngutnqls6909@gmail.com>
2024-09-03 02:19:02 +0900

    Feat: 게시글 리뷰 리스트 조회

---
+++ lib/data/model/review.dart
@@ -0,0 +1,40 @@
+
+/// 리뷰 모델
+class Review {
+  final int boardReviewId;
+  final int boardId;
+  final int userId;
+  final String nickName;
+  final String title;
+  final String comment;
+  final List<String> keywords;
+  final bool isLike;
+  final bool isDislike;
+
+  Review({
+    required this.boardReviewId,
+    required this.boardId,
+    required this.userId,
+    required this.nickName,
+    required this.title,
+    required this.comment,
+    required this.keywords,
+    required this.isLike,
+    required this.isDislike,
+  });
+
+  factory Review.fromJson(Map<String, dynamic> json) {
+    return Review(
+      boardReviewId: json['boardReviewId'] ?? 0,
+      boardId: json['boardId'] ?? 0,
+      userId: json['userId'] ?? 0,
+      nickName: json['nickname'] ?? '',
+      title: json['title'] ?? '',
+      comment: json['comment'] ?? '',
+      keywords:
+          (json['keywords'] as List).map((item) => item.toString()).toList(),
+      isLike: json['isLike'] ?? false,
+      isDislike: json['isDislike'] ?? false,
+    );
+  }
+}
---
+++ lib/data/model/review_response_model.dart
@@ -0,0 +1,17 @@
+import 'package:yangjataekil/data/model/review.dart';
+
+/// 리뷰 리스트 응답 모델
+class ReviewResponseModel {
+  final List<Review> reviews;
+
+  ReviewResponseModel({required this.reviews});
+
+  factory ReviewResponseModel.fromJson(Map<String, dynamic> json) {
+    var reviewJson = json['reviews'] as List;
+    List<Review> reviewList = reviewJson.map((i) => Review.fromJson(i)).toList();
+
+    return ReviewResponseModel(
+      reviews: reviewList,
+    );
+  }
+}
